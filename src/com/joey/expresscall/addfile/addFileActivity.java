dc497ed120a96a7fba9b4bb384abd364755f0c69
Joey <zhouye@jovision.com>
2016-04-21 18:12:34 +0800

    添加主页的文件条目

---
+++ src/com/joey/expresscall/addfile/addFileActivity.java
@@ -0,0 +1,31 @@
+package com.joey.expresscall.addfile;
+
+import com.joey.general.BaseActivity;
+
+public class addFileActivity extends BaseActivity{
+
+	@Override
+	protected void initSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initUi() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void saveSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void freeMe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
