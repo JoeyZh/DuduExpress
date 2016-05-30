af47eb90565ce1162e08afe24f5d8e200a869e1d
Joey <lma_ma@163.com>
2016-05-30 23:53:21 +0800

    添加下载接口

--- src/com/joey/expresscall/account/ECCallManager.java
+++ src/com/joey/expresscall/account/ECCallManager.java
@@ -12,103 +12,126 @@ import java.util.List;
 
 public class ECCallManager {
 
-    private static ECCallManager callManager;
-    private ECCallInterface callInterface;
+	private static ECCallManager callManager;
+	private ECCallInterface callInterface;
 
-    private ECCallManager() {
-    }
+	private ECCallManager() {
+	}
 
-    public static ECCallManager getInstance() {
-        if (callManager != null)
-            return callManager;
-        synchronized (ECAccountManager.class) {
-            if (callManager == null)
-                callManager = new ECCallManager();
-            return callManager;
-        }
-    }
+	public static ECCallManager getInstance() {
+		if (callManager != null)
+			return callManager;
+		synchronized (ECAccountManager.class) {
+			if (callManager == null)
+				callManager = new ECCallManager();
+			return callManager;
+		}
+	}
 
-    public void init(String token) {
-        callInterface = new ECCallInterface(token);
-    }
+	public void init(String token) {
+		callInterface = new ECCallInterface(token);
+	}
 
-    public <T> void getCallListDetail(final String callListId,ResponseListener<T> listener) {
-        TaskBuilder task = new TaskBuilder("callListDetail", listener, new OnTaskListener() {
-            @Override
-            public String execute() {
-                return callInterface.getCallListDetail(callListId);
-            }
-        });
+	public <T> void getCallListDetail(final String callListId,
+			ResponseListener<T> listener) {
+		TaskBuilder task = new TaskBuilder("callListDetail", listener,
+				new OnTaskListener() {
+					@Override
+					public String execute() {
+						return callInterface.getCallListDetail(callListId);
+					}
+				});
 
-        BackgroundHandler.execute(task);
-    }
+		BackgroundHandler.execute(task);
+	}
 
-    public <T> void getCallDetail(final String callId, ResponseListener<T> listener) {
-        TaskBuilder task = new TaskBuilder("callDetail", listener, new OnTaskListener() {
-            @Override
-            public String execute() {
-                return callInterface.getBillDetail(callId);
-            }
-        });
+	public <T> void getCallDetail(final String callId,
+			ResponseListener<T> listener) {
+		TaskBuilder task = new TaskBuilder("callDetail", listener,
+				new OnTaskListener() {
+					@Override
+					public String execute() {
+						return callInterface.getBillDetail(callId);
+					}
+				});
 
-        BackgroundHandler.execute(task);
-    }
+		BackgroundHandler.execute(task);
+	}
 
-    public <T> void getCalls(final int pageNum, final int pageSize, ResponseListener<T> listener) {
-        TaskBuilder task = new TaskBuilder("callList", listener, new OnTaskListener() {
-            @Override
-            public String execute() {
-                return callInterface.getBillList();
-            }
-        });
+	public <T> void getCalls(final int pageNum, final int pageSize,
+			ResponseListener<T> listener) {
+		TaskBuilder task = new TaskBuilder("callList", listener,
+				new OnTaskListener() {
+					@Override
+					public String execute() {
+						return callInterface.getBillList();
+					}
+				});
 
-        BackgroundHandler.execute(task);
-    }
+		BackgroundHandler.execute(task);
+	}
 
-    public <T> void getGroupCallList(final int pageNum, final int pageSize, ResponseListener<T> listener) {
-        TaskBuilder task = new TaskBuilder("groupCallList", listener, new OnTaskListener() {
-            @Override
-            public String execute() {
-                return callInterface.getCallList(pageSize, pageNum);
-            }
-        });
+	public <T> void getGroupCallList(final int pageNum, final int pageSize,
+			ResponseListener<T> listener) {
+		TaskBuilder task = new TaskBuilder("groupCallList", listener,
+				new OnTaskListener() {
+					@Override
+					public String execute() {
+						return callInterface.getCallList(pageSize, pageNum);
+					}
+				});
 
-        BackgroundHandler.execute(task);
-    }
+		BackgroundHandler.execute(task);
+	}
 
-    public <T> void call(final String fileId, final String fileType, String extra, final List<String> phones, ResponseListener<T> listener) {
-        TaskBuilder task = new TaskBuilder("call", listener, new OnTaskListener() {
-            @Override
-            public String execute() {
-                return callInterface.callArray(fileId, fileType, phones);
-            }
-        });
-        BackgroundHandler.execute(task);
-    }
+	public <T> void call(final String fileId, final String fileType,
+			String extra, final List<String> phones,
+			ResponseListener<T> listener) {
+		TaskBuilder task = new TaskBuilder("call", listener,
+				new OnTaskListener() {
+					@Override
+					public String execute() {
+						return callInterface
+								.callArray(fileId, fileType, phones);
+					}
+				});
+		BackgroundHandler.execute(task);
+	}
 
-    public <T> void getFileList(ResponseListener<T> listener){
-        TaskBuilder task = new TaskBuilder("files", listener, new OnTaskListener() {
-            @Override
-            public String execute() {
-                return callInterface.getFiles();
-            }
-        });
-        BackgroundHandler.execute(task);
-    }
+	public <T> void getFileList(ResponseListener<T> listener) {
+		TaskBuilder task = new TaskBuilder("files", listener,
+				new OnTaskListener() {
+					@Override
+					public String execute() {
+						return callInterface.getFiles();
+					}
+				});
+		BackgroundHandler.execute(task);
+	}
 
-    public <T> void deleteFile(final String fileId){
+	public <T> void deleteFile(final String fileId) {
 
-    }
+	}
 
-    public <T> void updateFile(final String fileId,String extraName,ResponseListener<T> listener){
-        TaskBuilder task = new TaskBuilder("files", listener, new OnTaskListener() {
-            @Override
-            public String execute() {
-                return callInterface.getFiles();
-            }
-        });
-        BackgroundHandler.execute(task);
-    }
-//	public boolean UpLoadCallFile()
+	public <T> void updateFile(final String fileId, String extraName,
+			ResponseListener<T> listener) {
+		TaskBuilder task = new TaskBuilder("files", listener,
+				new OnTaskListener() {
+					@Override
+					public String execute() {
+						return callInterface.getFiles();
+					}
+				});
+		BackgroundHandler.execute(task);
+	}
+
+	public boolean upLoadCallFile(String path, String phone, String type,
+			String extraName) {
+		return callInterface.upLoadFile(path, phone, type, extraName);
+	}
+
+	public boolean downloadFile(String fileId, String type, String path) {
+		return callInterface.downloadFile(fileId, type, path);
+	}
 
 }
--- src/com/joey/expresscall/addfile/ECSaveFileActivity.java
+++ src/com/joey/expresscall/addfile/ECSaveFileActivity.java
@@ -1,33 +1,40 @@
 package com.joey.expresscall.addfile;
 
+import com.joey.expresscall.R;
+import com.joey.expresscall.account.ECCallManager;
+import com.joey.expresscall.file.bean.FileBean;
 import com.joey.general.BaseActivity;
 
 /**
  * Created by Joey on 2016/5/23.
  */
-public class ECSaveFileActivity extends BaseActivity{
+public class ECSaveFileActivity extends BaseActivity {
+
+	private String path;
 
 	@Override
 	public void initSettings() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void initUi() {
-		// TODO Auto-generated method stub
-		
+		setContentView(R.layout.activity_save_file);
 	}
 
 	@Override
 	public void saveSettings() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void freeMe() {
 		// TODO Auto-generated method stub
-		
+
+	}
+	
+	private void upLoadFile(){
+		ECCallManager.getInstance().
 	}
 }
